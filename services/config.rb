b6f14201d8d0190465bffcf5202caf21bfd66e9c
mityakg <gumennikov.d@gmail.com>
2016-12-16 11:43:49 +0600

    some more changes to config file

--- services/config.rb
+++ services/config.rb
@@ -17,6 +17,8 @@ coreo_aws_advisor_alert "test_alert" do
 	display_name "Heartbeat check"
 	description "This check finds out what load balancers have healthcheck intervals > 2 mins"
 	level "Informational"
+	category "Inventory"
+	suggested_action "None."
 	objectives ["load_balancers"]
 	audit_objects["load_balancer_description.health_check.interval"]
 	operators [">"]
